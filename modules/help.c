838017000f7978e5de99134653841643940d7720
Erfan Mola <hi@erfanxd.ir>
2023-04-27 10:13:49 +0330

    Updated command info

--- modules/help.c
+++ modules/help.c
@@ -15,7 +15,7 @@ static void show_help(void) {
     fprintf(stderr, "\033[36m\033[1mUsage:\033[0m\033[0m " PROGNAME " [command] [options]\n\n"
         "\033[36m\033[1mbalance     \033[0m\033[0m - retrieves the balance of the wallet\n"
         "\033[36m\033[1mhelp        \033[0m\033[0m - shows this help menu\n"
-        "\033[36m\033[1mrevoke      \033[0m\033[0m - revokes the secret of the wallet\n"
+        "\033[36m\033[1mrevoke      \033[0m\033[0m - revokes the secret of the wallet and returns the new secret\n"
         "\033[36m\033[1mtransaction \033[0m\033[0m - retrieves information of the given transaction id\n"
         "\033[36m\033[1mtransactions\033[0m\033[0m - retrieves transactions in descending order from specified offset (default is 0)\n"
         "\033[36m\033[1mtransfer    \033[0m\033[0m - transfers specified amount of DPX from departure wallet to destination wallet\n"
